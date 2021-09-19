6cf2e13ed983b0f5af31aa79ff76eb429cec1730
thuc nhi <thuc-nhi.wiedenhofer@laolateforme.io>
2021-09-19 11:46:23 +0200

    save finit

--- calculator9000/src/components/NumberProvider.js
+++ calculator9000/src/components/NumberProvider.js
@@ -127,6 +127,7 @@ const NumberProvider = props => {
         operation:operation})
         .then((response)=>{
           setPost(response.data);
+          console.log(post);
           handleClearCalcul();
         });
     }
--- calculator9000/src/components/SaveCalcul.js
+++ calculator9000/src/components/SaveCalcul.js
@@ -1,13 +1,10 @@
 import React, { useContext } from 'react';
 import { NumberContext } from './NumberProvider';
-import WonderSave from './WonderfulSave';
 
 const SaveCalcul = () => {
-    const { result } = useContext(NumberContext);
-    const { operation} = useContext(NumberContext);
-   
-    
-        return (<div>{operation} {result}</div>);
-   
+    const { post } = useContext(NumberContext);
+    if(post!== null){
+        return(<div>{post.operation} {post.result}</div>)
+    }
 };
 export default SaveCalcul;
